3537ef32b84c7ea0a35646e1e4fa85574397f28e
zack781 <thean781@gmail.com>
2024-08-10 13:11:26 -0400

    first commit

---
+++ essentials/essentials/essentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  essentialsApp.swift
+//  essentials
+//
+//  Created by Zack Nguyen on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct essentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
